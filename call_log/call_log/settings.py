34950fbf6ea5062c9e2b4b4ca76fcbc4689ea643
Baghaii <baghaii@gmail.com>
2015-05-04 09:46:25 -0600

    Adding the correct directory

---
+++ call_log/call_log/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for call_log project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'call_log'
+
+SPIDER_MODULES = ['call_log.spiders']
+NEWSPIDER_MODULE = 'call_log.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'call_log (+http://www.yourdomain.com)'
---
+++ call_log/call_log/spiders/call_log_spider.py
@@ -0,0 +1,35 @@
+import scrapy
+import time
+from call_log.items import CallLogItem
+
+from datetime import date,timedelta
+
+#use todays date to build a URL
+def build_urls():
+	urls = []
+	base = "https://bouldercolorado.gov/pages/call-log-"
+
+	#Build URLs for the last two weeks
+	for i in range(1, 14):
+		today = date.today() - timedelta(days=i)
+		str_date = today.strftime("%B-%d-%Y")
+		url = base + str_date
+		urls.append(url)
+	return urls
+
+class CallSpider(scrapy.Spider):
+	name = "call"
+	allowed_domains = ["bouldercolorado.gov"]
+	start_urls = build_urls()
+
+#extracts the text of a given format from the page
+	def parse(self, response):
+		for sel in response.xpath('//tr'):
+			item = CallLogItem()
+			[case, date, time, loc, call_type]  = sel.xpath('td/text()').extract()
+			item['case'] = case
+			item['date'] = date
+			item['time'] = time
+			item['loc']  = loc
+			item['call_type'] = call_type
+			yield item
